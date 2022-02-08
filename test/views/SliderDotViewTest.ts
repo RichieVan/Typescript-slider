b4c438010bf1b44a02c92795779dd33328df4173
RichieVan <richievan2@mail.ru>
2022-02-08 19:15:37 +0500

    Удаление jsdom из теста

---
+++ test/views/SliderDotViewTest.ts
@@ -0,0 +1,12 @@
+import {assert} from "chai";
+
+import SliderDotView from "../../lib/js/views/SliderDotView";
+
+
+describe('Первый тест', function () {
+  const sliderDotView = new SliderDotView();
+
+  it('Сабтест', function () {
+    assert.equal(sliderDotView.render(), 'hello');
+  })
+})
