3f3ffbafb0bb896e05cd52964287af1dd63c6b56
RichieVan <richievan2@mail.ru>
2022-02-08 19:16:26 +0500

    Добавление классов компонентов

---
+++ lib/js/models/SliderModel.ts
@@ -0,0 +1,5 @@
+class SliderModel {
+
+}
+
+export default SliderModel;
--- lib/js/views/SliderDotView.ts
+++ lib/js/views/SliderDotView.ts
@@ -1,10 +1,76 @@
-class SliderDotView {
+import * as $ from 'jquery';
+import ISliderDotView from '../../interface/ISliderDotView';
+
+class SliderDotView implements ISliderDotView {
+  element: JQuery;
+
+  active: boolean;
+
+  shift: number;
+
   constructor() {
     
   }
 
-  render() {
-    return 'hello1';
+  setActive(state) {
+    this.active = state;
+  }
+
+  setShift(state) {
+    this.shift = state;
+  }
+
+  getRect() {
+    return this.element[0].getBoundingClientRect();
+  }
+
+  getElement() {
+    return this.element;
+  }
+
+  mouseDownHandler(e) {
+    this.setActive(true);
+    this.setShift(e.offsetX + 8);
+  }
+
+  mouseUpHandler() {
+    if (this.active) {
+      this.setActive(false);
+      this.setShift(0);
+    }
+  }
+
+  mouseMoveHandler(e) {
+    if (this.active) {
+      const pos = e.clientX - this.shift;
+      this.element.css({ left: `${pos}px` });
+    }
+  }
+
+  renderElement(): JQuery {
+    const htmlElement = $('<div/>', {
+      class: 'slider__dot',
+      draggable: false,
+    });
+
+    this.element.on('dragstart', () => false);
+
+    this.element.on('mousedown', (e) => {
+      this.mouseDownHandler(e);
+    });
+
+    $(document).on('mouseup', () => {
+      this.mouseUpHandler();
+    });
+
+    $(document).on('mousemove', (e) => {
+      if (this.active) {
+        this.mouseMoveHandler(e);
+      }
+    });
+
+    this.element = htmlElement;
+    return this.element;
   }
 }
 
