9ff615fe315703bc89073d1bcb1427b159dda246
RichieVan <richievan2@mail.ru>
2022-02-10 15:33:28 +0500

    Стартовая реализация конструктора модели

--- lib/js/models/SliderModel.ts
+++ lib/js/models/SliderModel.ts
@@ -1,14 +1,28 @@
 import ISliderModel from "../../interface/ISliderModel";
+import SliderProps from "../../type/SliderProps";
 
 class SliderModel implements ISliderModel {
   min: number;
 
   max: number;
 
+  step: number;
+
   value: number;
 
-  constructor() {
-    
+  constructor(props: SliderProps) {
+    this.min = props.min;
+    this.max = props.max;
+    this.step = props.step || ((this.max - this.min) / 100);
+    this.value = props.current || 0;
+
+    if (this.min >= this.max) {
+      throw new Error('Минимальное значение не может быть больше максимального');
+    }
+
+    if (this.step > (this.max - this.min)) {
+      throw new Error('Шаг не может быть больше разницы минимального и максимального значения');
+    }
   }
 }
 
