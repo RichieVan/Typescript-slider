feb498e2eebed91a3a90e136f58f6c2567f013ef
RichieVan <richievan2@mail.ru>
2022-02-10 18:00:44 +0500

    Изменение линтера и tsconfig

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,11 +1,22 @@
 module.exports = {
   extends: [
     'airbnb-base',
-    'airbnb-typescript/base',
   ],
+  parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaVersion: 'latest',
     sourceType: 'module',
+  },
+  plugins: ['@typescript-eslint'],
+  overrides: {
+    files: ['*.ts'],
+    extends: [
+      'airbnb-typescript/base',
+      'plugin:@typescript-eslint/recommended'
+    ],
+    parserOptions: {
+      project: ['./tsconfig.json'],
+    }
   },
   rules: {
     'func-names': 'off',
