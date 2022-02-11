020f5546c3b91898a4a138b28d1e35f949d57f17
RichieVan <richievan2@mail.ru>
2022-02-11 17:26:10 +0500

    Добавление новых тестов для model и presenter

--- test/models/SliderModelTest.ts
+++ test/models/SliderModelTest.ts
@@ -1,4 +1,4 @@
-import { expect } from 'chai';
+import { expect, assert } from 'chai';
 import ISliderModel from '../../lib/interface/ISliderModel';
 import SliderProps from '../../lib/type/SliderProps';
 import SliderModel from '../../lib/js/models/SliderModel';
@@ -8,35 +8,78 @@ type ModelInstance = null | ISliderModel;
 let modelInstance: ModelInstance = null;
 
 const SliderModelTests = function () {
-  describe('Тестирование присваивания пропсов', () => {
-    it('Выдать ошибку если значение min больше чем значение max', () => {
-      const modelProps: SliderProps = {
-        min: 5,
-        max: 3,
-        step: 1,
-      };
+  describe('Тесты model', () => {
+    describe('Валидация присваивания пропсов при инициализации', () => {
+      it('Выдать ошибку если значение min больше чем значение max', () => {
+        const modelProps: SliderProps = {
+          min: 5,
+          max: 3,
+          step: 1,
+        };
 
-      const throwableFunction = function (): ISliderModel {
-        modelInstance = new SliderModel(modelProps);
-        return modelInstance;
-      };
+        const throwableFunction = function (): ISliderModel {
+          modelInstance = new SliderModel(modelProps);
+          return modelInstance;
+        };
 
-      expect(throwableFunction).to.throw('Минимальное значение не может быть больше максимального');
+        expect(throwableFunction).to.throw('Минимальное значение не может быть больше максимального');
+      });
+
+      it('Выдать ошибку если значение step больше разницы значений min и max', () => {
+        const modelProps: SliderProps = {
+          min: 3,
+          max: 5,
+          step: 3,
+        };
+
+        const throwableFunction = function (): ISliderModel {
+          modelInstance = new SliderModel(modelProps);
+          return modelInstance;
+        };
+
+        expect(throwableFunction).to.throw('Шаг не может быть больше разницы минимального и максимального значения');
+      });
     });
 
-    it('Выдать ошибку если значение step больше разницы значений min и max', () => {
-      const modelProps: SliderProps = {
-        min: 3,
-        max: 5,
-        step: 3,
-      };
+    describe('Получение ближайшего значения из списка', () => {
+      it('Вернуть индекс одного значения из списка ближайшего к 4', () => {
+        const modelProps: SliderProps = {
+          min: 0,
+          max: 10,
+          step: 1,
+          values: [0, 10],
+        };
 
-      const throwableFunction = function (): ISliderModel {
         modelInstance = new SliderModel(modelProps);
-        return modelInstance;
-      };
 
-      expect(throwableFunction).to.throw('Шаг не может быть больше разницы минимального и максимального значения');
+        assert.equal(modelInstance.getIndexOfClosestValue(4), 0);
+      });
+
+      it('Вернуть индекс одного значения из списка ближайшего к 5', () => {
+        const modelProps: SliderProps = {
+          min: 0,
+          max: 10,
+          step: 1,
+          values: [0, 10],
+        };
+
+        modelInstance = new SliderModel(modelProps);
+
+        assert.equal(modelInstance.getIndexOfClosestValue(5), 1);
+      });
+
+      it('Вернуть индекс одного значения из списка ближайшего к 8', () => {
+        const modelProps: SliderProps = {
+          min: 0,
+          max: 10,
+          step: 1,
+          values: [0, 10],
+        };
+
+        modelInstance = new SliderModel(modelProps);
+
+        assert.equal(modelInstance.getIndexOfClosestValue(8), 1);
+      });
     });
   });
 };
--- test/presenters/SliderPresenterTest.ts
+++ test/presenters/SliderPresenterTest.ts
@@ -1,32 +1,70 @@
-import {assert, expect} from "chai";
-import ISliderPresenter from "../../lib/interface/ISliderPresenter";
+import { assert, expect } from 'chai';
+import ISliderPresenter from '../../lib/interface/ISliderPresenter';
 import SliderPresenter from '../../lib/js/presenters/SliderPresenter';
 
-type sliderInstanceType = null | ISliderPresenter;
+type PresenterInstance = null | ISliderPresenter;
 
-let sliderInstance: sliderInstanceType = null;
+let presenter: PresenterInstance = null;
 
 const SliderPresenterTests = function () {
-  // describe('Тестирование присваивания пропсов', function () {
-  //   this.afterEach(function () {
-  //     if (sliderInstance) {
-  //       sliderInstance.destroy();
-  //       sliderInstance = null;
-  //     }
-  //   })
+  describe('Тесты presenter\'а', () => {
+    describe('Конвертирование DOM координат', () => {
+      afterEach(() => {
+        presenter?.destroy();
+        presenter = null;
+      });
 
-  //   it('Проброс ошибки если min больше чем max', function () {
-  //     const throwableFunction = function(): void {
-  //       sliderInstance = new SliderPresenter({
-  //         min: 5,
-  //         max: 3,
-  //         step: 1
-  //       });
-  //     }
+      it('Преобразовать DOM координату 200 в значение слайдера 4', () => {
+        const pos = 200;
+        presenter = new SliderPresenter({
+          min: 0,
+          max: 10,
+        });
 
-  //     expect(throwableFunction).to.throw();
-  //   })
-  // })
-}
+        assert.equal(presenter.convertDOMPosToSliderValue(pos), 4);
+      });
+
+      it('Преобразовать DOM координату 300 в значение слайдера 6', () => {
+        const pos = 300;
+        presenter = new SliderPresenter({
+          min: 0,
+          max: 10,
+        });
+
+        assert.equal(presenter.convertDOMPosToSliderValue(pos), 6);
+      });
+
+      it('Преобразовать DOM координату 333 в значение слайдера 6.66', () => {
+        const pos = 333;
+        presenter = new SliderPresenter({
+          min: 0,
+          max: 10,
+        });
+
+        assert.equal(presenter.convertDOMPosToSliderValue(pos), 6.66);
+      });
+
+      it('Преобразовать DOM координату 333 в значение слайдера 13.32', () => {
+        const pos = 333;
+        presenter = new SliderPresenter({
+          min: 0,
+          max: 20,
+        });
+
+        assert.equal(presenter.convertDOMPosToSliderValue(pos), 13.32);
+      });
+
+      it('Преобразовать DOM координату 500 в значение слайдера 37', () => {
+        const pos = 500;
+        presenter = new SliderPresenter({
+          min: 0,
+          max: 37,
+        });
+
+        assert.equal(presenter.convertDOMPosToSliderValue(pos), 37);
+      });
+    });
+  });
+};
 
 export default SliderPresenterTests;
