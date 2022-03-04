dccc0ba35050f090b70d0eb51802c61226987938
RichieVan <richievan2@mail.ru>
2022-03-04 16:01:19 +0500

    Переименование классов слайдера

--- lib/js/helpers/DOMHelper.ts
+++ lib/js/helpers/DOMHelper.ts
@@ -3,40 +3,40 @@ import $ from 'jquery';
 class DOMHelper {
   public static createSliderElement(): JQuery<HTMLElement> {
     const element = $('<div/>', {
-      class: 'slider',
+      class: 'rv-slider',
     });
     return element;
   }
 
   public static createControlsElement(): JQuery<HTMLElement> {
     const element = $('<div/>', {
-      class: 'slider__controls',
+      class: 'rv-slider__controls',
     });
     return element;
   }
 
   public static createWrapperElement(): JQuery<HTMLElement> {
     const element = $('<div/>', {
-      class: 'slider__wrapper',
+      class: 'rv-slider__wrapper',
     });
     return element;
   }
 
   public static createRangeElement(): JQuery<HTMLElement>[] {
     const wrapper = $('<div/>', {
-      class: 'slider__range-wrapper',
+      class: 'rv-slider__range-wrapper',
     });
 
     const inner = $('<div/>', {
-      class: 'slider__range-inner',
+      class: 'rv-slider__range-inner',
     });
 
     const range = $('<div/>', {
-      class: 'slider__range',
+      class: 'rv-slider__range',
     });
 
     const progress = $('<div/>', {
-      class: 'slider__progress',
+      class: 'rv-slider__progress',
     });
 
     inner.append([range, progress]);
@@ -46,11 +46,11 @@ class DOMHelper {
 
   public static createThumbElement(): JQuery<HTMLElement>[] {
     const element = $('<div/>', {
-      class: 'slider__thumb',
+      class: 'rv-slider__thumb',
     });
 
     const wrapper = $('<div/>', {
-      class: 'slider__thumb-wrapper',
+      class: 'rv-slider__thumb-wrapper',
     });
 
     element.append(wrapper);
@@ -60,22 +60,22 @@ class DOMHelper {
 
   public static createThumbContentElement(): JQuery<HTMLElement> {
     const element = $('<div/>', {
-      class: 'slider__thumb-content',
+      class: 'rv-slider__thumb-content',
     });
     return element;
   }
 
   public static createThumbMarkElement(): JQuery<HTMLElement>[] {
     const element = $('<div/>', {
-      class: 'slider__thumb-mark',
+      class: 'rv-slider__thumb-mark',
     });
 
     const value = $('<div/>', {
-      class: 'slider__thumb-value',
+      class: 'rv-slider__thumb-value',
     });
 
     const marker = $('<div/>', {
-      class: 'slider__thumb-marker',
+      class: 'rv-slider__thumb-marker',
     });
 
     element.append(value, marker);
@@ -85,7 +85,7 @@ class DOMHelper {
 
   public static createDivisionsContainerElement(): JQuery<HTMLElement> {
     const element = $('<div/>', {
-      class: 'slider__divisions',
+      class: 'rv-slider__divisions',
     });
 
     return element;
@@ -93,21 +93,21 @@ class DOMHelper {
 
   public static createDivisionElement(val: number): JQuery<HTMLElement> {
     const element = $('<div/>', {
-      class: 'slider__division',
+      class: 'rv-slider__division',
     });
 
     const wrapper = $('<div/>', {
-      class: 'slider__division-wrapper',
+      class: 'rv-slider__division-wrapper',
     });
 
     element.append(wrapper);
 
     const marker = $('<div/>', {
-      class: 'slider__division-marker',
+      class: 'rv-slider__division-marker',
     });
 
     const value = $('<div/>', {
-      class: 'slider__division-value',
+      class: 'rv-slider__division-value',
       html: String(val),
     });
 
@@ -117,23 +117,23 @@ class DOMHelper {
   }
 
   public static getProgressBarEmptyClass(): string {
-    return 'slider__progress_empty';
+    return 'rv-slider__progress_empty';
   }
 
   public static getProgressBarFullClass(): string {
-    return 'slider__progress_full';
+    return 'rv-slider__progress_full';
   }
 
   public static getThumbActiveClass(): string {
-    return 'slider__thumb_active';
+    return 'rv-slider__thumb_active';
   }
 
   public static getThumbViewMouseUpEventClass(): string {
-    return 'slider_moving_to_valid';
+    return 'rv-slider_moving_to_valid';
   }
 
   public static getEnabledThumbMarksModifierClass(): string {
-    return 'slider_with_thumb-mark';
+    return 'rv-slider_with_thumb-mark';
   }
 }
 
