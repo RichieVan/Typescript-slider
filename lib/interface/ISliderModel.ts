2b4ecd9340c890490d3868f5793fdbed26cb94c9
RichieVan <richievan2@mail.ru>
2022-03-07 19:03:47 +0500

    Обновление интерфесов модели

--- lib/interface/ISliderModel.ts
+++ lib/interface/ISliderModel.ts
@@ -3,7 +3,17 @@ import SliderViewProps from '../type/SliderViewProps';
 
 interface ISliderModel {
   setThumbsValues(thumbs: number[]): void;
+  setMin(val: number): void
+  setMax(val: number): void
+  setStep(val: number): void;
+  setSmooth(val: boolean): void;
+  setRange(val: boolean): void;
+  setShowThumbValue(val: boolean): void;
+  setShowMarks(val: boolean): void;
+  setShowMinAndMax(val: boolean): void;
+  setVertical(val: boolean): void
   getMin(): number;
+  getMax(): number;
   getStep(): number;
   getLength(): number;
   getValues(): number[];
